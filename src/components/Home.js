067a978994c8e0e818da3fe63da572c5a0b10943
rkp1819 <rkp1819@gmail.com>
2020-09-29 12:30:02 +0530

    city Search

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,51 +5,90 @@ import axios from "axios";
 import { InputBase } from "@material-ui/core";
 
 const config = {
-  headers: { "user-key": "" },
+  headers: { "user-key": "8cd455d78407927ed585bf555240c308" },
 };
 
 function Home() {
-  const [city, setCity] = useState("Hyderabad");
+  const [cities, setCities] = useState([]);
+  const [city, setCity] = useState({});
 
+  const [cityInput, setCityInput] = useState("");
   useEffect(() => {
     axios
-      .get(`https://developers.zomato.com/api/v2.1/cities?q=${city}`, config)
-      .then((res) => console.log(res.data.location_suggestions))
+      .get(
+        `https://developers.zomato.com/api/v2.1/cities?q=${city.name}`,
+        config
+      )
+      .then((res) => {
+        setCities(res.data.location_suggestions);
+      })
       .catch((err) => {
         alert(err.msg);
       });
   }, [city]);
 
+  function handleChangeCity(event) {
+    let val = event.target.value;
+    console.log("in handleChangeCity " + event.key);
+    setCityInput(val);
+  }
+
   function changeCity(event) {
     console.log(event.key);
     let val = event.target.value;
-    if (event.key == "Enter") {
-      setCity(val);
+    if (event.key == "Enter" && val) {
+      setCity({ ...city, name: val });
     }
+  }
+
+  function selectCity(item) {
+    console.log("selected City " + item.name, item);
+    setCity(item);
+    setCityInput("");
   }
 
   return (
     <div className="home_root">
       <h1 className="home_root_header">Zomato Clone</h1>
       <p className="home_root_helper">
-        Discover the best food & drinks in {city}
+        Discover the best food & drinks in{" "}
+        {city.name
+          ? city.name.trim() == cityInput.trim()
+            ? "Your city"
+            : city.name
+          : "Your city"}
       </p>
-
-      <div className="home_root_search">
-        <div className="home_root_search_cityselector">
-          <InputBase
-            autoFocus={true}
-            onKeyPress={changeCity}
-            placeholder="Location"
-            required={true}
-          />
+      <div className="home_root_search_container">
+        <div className="home_root_search">
+          <div className="home_root_search_cityselector">
+            <InputBase
+              autoFocus={true}
+              onKeyPress={changeCity}
+              placeholder="Location"
+              required={true}
+              value={cityInput ? cityInput : ""}
+              onChange={handleChangeCity}
+            />
+          </div>
+          <div className="home_root_search_bar">
+            <InputBase
+              placeholder="Search for restaurant, cuisine or a dish"
+              required={true}
+            />
+          </div>
+          <br />
         </div>
-        <div className="home_root_search_bar">
-          <InputBase
-            placeholder="Search for restaurant, cuisine or a dish"
-            required={true}
-          />
-        </div>
+        {cities.map((item, index) => {
+          return (
+            <div
+              key={index}
+              onClick={() => selectCity(item)}
+              className="home_root_search_cityview"
+            >
+              {item.name}
+            </div>
+          );
+        })}
       </div>
     </div>
   );
